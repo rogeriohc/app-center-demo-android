f5dcbabab88de48cc8cd2990c656becfd3a1355f
rogeriohc <rogeriohc@hotmail.com>
2018-12-12 10:15:18 -0200

    Teste 3.

--- AppCenterDemoAndroid/MainActivity.cs
+++ AppCenterDemoAndroid/MainActivity.cs
@@ -19,6 +19,7 @@ namespace AppCenterDemoAndroid
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
+            //App Center Start 
             AppCenter.Start("88d2c395-566e-46e1-a5ef-a8ce69e2c7f2", typeof(Analytics), typeof(Crashes));
 
             // Get our button from the layout resource,
