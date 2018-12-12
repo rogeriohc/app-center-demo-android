86d05167429504367f38023c07c86d0aaa0e2f78
rogeriohc <rogeriohc@hotmail.com>
2018-12-12 12:02:33 -0200

    Suporte ao Push.

--- AppCenterDemoAndroid/MainActivity.cs
+++ AppCenterDemoAndroid/MainActivity.cs
@@ -4,6 +4,8 @@ using Android.OS;
 using Microsoft.AppCenter;
 using Microsoft.AppCenter.Analytics;
 using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter.Push;
+
 
 namespace AppCenterDemoAndroid
 {
@@ -19,8 +21,37 @@ namespace AppCenterDemoAndroid
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            //App Center Start 
-            AppCenter.Start("88d2c395-566e-46e1-a5ef-a8ce69e2c7f2", typeof(Analytics), typeof(Crashes));
+            //App Center Analytics and Crashes 
+            //AppCenter.Start("88d2c395-566e-46e1-a5ef-a8ce69e2c7f2", typeof(Analytics), typeof(Crashes));
+
+            //App Center Push
+            if (!AppCenter.Configured)
+            {
+                Push.PushNotificationReceived += (sender, e) =>
+                {
+                    // Add the notification message and title to the message
+                    var summary = $"Push notification received:" +
+                                        $"\n\tNotification title: {e.Title}" +
+                                        $"\n\tMessage: {e.Message}";
+
+                    // If there is custom data associated with the notification,
+                    // print the entries
+                    if (e.CustomData != null)
+                    {
+                        summary += "\n\tCustom data:\n";
+                        foreach (var key in e.CustomData.Keys)
+                        {
+                            summary += $"\t\t{key} : {e.CustomData[key]}\n";
+                        }
+                    }
+
+                    // Send the notification summary to debug output
+                    
+                    System.Diagnostics.Debug.WriteLine(summary);
+                };
+            }
+
+            AppCenter.Start("88d2c395-566e-46e1-a5ef-a8ce69e2c7f2", typeof(Push), typeof(Analytics), typeof(Crashes));
 
             // Get our button from the layout resource,
             // and attach an event to it
